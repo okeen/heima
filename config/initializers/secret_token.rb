05d321379456d825f4402fc3ece057abfebc2fe5
okeen <eneko.taberna@gmail.com>
2012-02-01 11:12:20 +0100

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Heima::Application.config.secret_token = 'e4c322c0f5f3b933ec67c4b461aded5ff35fac6a4fca6f2cd8ac8b8d4781e3bc814604884568f28537902194ef5d65199746ddb4e26972f4713e023f15a8fcb8'
