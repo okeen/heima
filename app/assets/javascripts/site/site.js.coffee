e0810279f35f2e32b2c0efd499c1160b1b15e3b9
okeen <eneko.taberna@gmail.com>
2012-02-03 23:45:41 +0100

    Improved language bar

---
+++ app/assets/javascripts/site/site.js.coffee
@@ -0,0 +1,5 @@
+#= require jquery
+#= require jquery_ujs
+#= require site/json2
+#= require site/underscore
+#= require site/backbone
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,9 @@
 module ApplicationHelper
   
   def locale_flag(locale)
-    content_tag :li, :class => "locale" do
+    content_tag :li, 
+        :class => "locale #{'current_locale' if I18n.locale == locale}",
+        :style => I18n.locale == locale ? "" : "display:none" do
       link_to image_tag("flags/#{locale}.png"), :locale => locale
     end
   end
